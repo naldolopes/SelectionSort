f6a9ac1ec363ea00d7ed95e91b465539a7459947
Reginaldo Oliveira <reginaldo.l@gmail.com>
2023-09-12 20:28:08 -0300

    Implementation os Selection Sort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyProject;
+class Program
+{
+
+    static void SelectionSort(int[] array)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int min_index = i;
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[min_index])
+                {
+                    min_index = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        int[] array = { 4, 2, 6, 5, 1, 3 };
+
+        Console.WriteLine("\nArray before Selection Sort:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        SelectionSort(array);
+
+        Console.WriteLine("\nArray after Selection Sort:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+
+        Console.WriteLine("\n");
+    }
+}
